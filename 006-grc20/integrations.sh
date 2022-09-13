d5394ed29005e9105f28f49ce529bc318a9b2be5
Manfred Touron <94029+moul@users.noreply.github.com>
2022-09-13 16:02:14 +0000

    chore: update 006-grc20
    Signed-off-by: Manfred Touron <94029+moul@users.noreply.github.com>

--- 006-grc20/integrations.sh
+++ 006-grc20/integrations.sh
@@ -3,5 +3,6 @@
 . ../integrations.lib.sh
 
 pre
-call --pkgpath ${GNO_CONTRACT_ENDPOINT} --func "Render" --args ""
+#call --pkgpath ${GNO_CONTRACT_ENDPOINT} --func "FooToken.BalanceOf" --args "g1jg8mtutu9khhfwc4nxmuhcpftf0pajdhfvsqf5"
+call --pkgpath ${GNO_CONTRACT_ENDPOINT} --func "MyBalance"
 post
