241ca4402ea845ae3bb9f650db3c3cfff1544878
Manfred Touron <94029+moul@users.noreply.github.com>
2022-09-13 16:14:04 +0000

    chore: update 007-vault
    Signed-off-by: Manfred Touron <94029+moul@users.noreply.github.com>

--- 007-vault/integrations.sh
+++ 007-vault/integrations.sh
@@ -4,5 +4,6 @@
 
 pre
 #call --pkgpath ${GNO_CONTRACT_ENDPOINT} --func "FooToken.BalanceOf" --args "g1jg8mtutu9khhfwc4nxmuhcpftf0pajdhfvsqf5"
+call --pkgpath ${GNO_CONTRACT_ENDPOINT} --func "Deposit" --args "10000" --args "g1myrecoveryaddress"
 call --pkgpath ${GNO_CONTRACT_ENDPOINT} --func "MyBalance"
 post
